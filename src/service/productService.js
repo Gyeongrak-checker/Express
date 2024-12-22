38e85c4d0eb3360c65b334a63583265884afe021
윾승도 <yus174113@gmail.com>
2024-12-22 16:49:29 +0900

    feat: 품목 캐싱

--- src/service/productService.js
+++ src/service/productService.js
@@ -9,8 +9,31 @@ const save = async () => {
 
      const response = JSON.parse(mock);
 
-     console.log(response)
+     const result = [];
 
+
+     for(const product of response) {
+         const tmp = {};
+         if(product.mid === "00") {
+             tmp.rn = product.rn;
+             tmp.large = product.large;
+             tmp.largeName = product.largeName;
+             tmp.mid = [];
+
+             result.push(tmp);
+         } else if(product.small === "00") {
+             tmp.rn = product.rn;
+             tmp.mid = product.mid;
+             tmp.midName = product.midName;
+             tmp.small = [];
+             result[result.length - 1].mid.push(tmp);
+         } else {
+             tmp.rn = product.rn;
+             tmp.gooName = product.goodName;
+             result[result.length - 1].mid[result.mid.length -1].small.push(tmp);
+         }
+     }
+     return result;
 }
 
 
