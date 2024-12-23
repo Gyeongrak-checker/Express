07ea5ec656861b72d5488edf61fc33b5aebcd119
윾승도 <66948129+tmdeh@users.noreply.github.com>
2024-12-24 08:07:22 +0900

    feat: 품목 요청1

--- src/service/productService.js
+++ src/service/productService.js
@@ -1,17 +1,28 @@
 const openApi = require("../modules/open-api/request");
 const { Products } = require('../database/schema/productSchema');
 const fs = require('fs');
+
+const productCount = 17553 / 1000 + 1;
+
 const save = async () => {
 //   const response = await openApi.getProductCode();
      // TODO: 품목 캐싱
 
-     const mock = fs.readFileSync('./mock.json');
+//     const mock = fs.readFileSync('./mock.json');
 
-     const response = JSON.parse(mock);
+//     const response = JSON.parse(mock);
 
      const result = [];
 
-
+     // 1. 데이터 요청
+     const promiseArray = [];
+     for(let i = 1; i < productCount; i++) {
+         promiseArray.push(openApi.getProductCode(i).then(res => res.data));
+     }
+     let response = await Promise.all(response);
+     // 2. 데이터 정렬
+     response = response.sort((a, b) => a.rn - b.rn);
+     // 3. 데이터 구죠 변경
      for(const product of response) {
          const tmp = {};
          if(product.mid === "00") {
@@ -33,6 +44,9 @@ const save = async () => {
              result[result.length - 1].mid[result.mid.length -1].small.push(tmp);
          }
      }
+     // 4. MongoDB에 삽입
+
+
      return result;
 }
 
