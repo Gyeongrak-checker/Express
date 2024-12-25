48cbf5e617ee71887bede5877b504a091538f476
윾승도 <66948129+tmdeh@users.noreply.github.com>
2024-12-26 06:37:56 +0900

    feat: 품목 mongoDB에 캐싱

--- src/service/productService.js
+++ src/service/productService.js
@@ -5,25 +5,31 @@ const fs = require('fs');
 const productCount = 17553 / 1000 + 1;
 
 const save = async () => {
-//   const response = await openApi.getProductCode();
-     // TODO: 품목 캐싱
-
-//     const mock = fs.readFileSync('./mock.json');
-
-//     const response = JSON.parse(mock);
-
-     const result = [];
-
      // 1. 데이터 요청
      const promiseArray = [];
-     for(let i = 1; i < productCount; i++) {
+     for(let i = 1; i < 2; i++) {
          promiseArray.push(openApi.getProductCode(i).then(res => res.data));
      }
-     let response = await Promise.all(response);
+     let response = await Promise.all(promiseArray);
+
      // 2. 데이터 정렬
-     response = response.sort((a, b) => a.rn - b.rn);
+    response = response
+        .flat(2)
+        .sort((a, b) => a.rn - b.rn);
+
      // 3. 데이터 구죠 변경
-     for(const product of response) {
+
+     const result = toDocument(response);
+
+     // 4. MongoDB에 삽입
+     await Products.insertMany(result);
+
+    return response;
+}
+
+const toDocument = (array) => {
+    const result = [];
+    for(const product of array) {
          const tmp = {};
          if(product.mid === "00") {
              tmp.rn = product.rn;
@@ -44,17 +50,13 @@ const save = async () => {
              result[result.length - 1].mid[result.mid.length -1].small.push(tmp);
          }
      }
-     // 4. MongoDB에 삽입
-
-
-     return result;
+    return result;
 }
 
-
 const get = async () => {
-   const products = await Products.find();
+   let products = await Products.find();
    if(products.length === 0) {
-      await save();
+      products = await save();
    }
    return products;
 }
