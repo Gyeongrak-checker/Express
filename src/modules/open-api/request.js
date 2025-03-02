fb6d02c635797f237a862afc4943117c8bf77fdb
윾승도 <66948129+tmdeh@users.noreply.github.com>
2025-03-02 17:21:17 +0900

    feat: 도매시장 코드 조회 모듈 테스트

--- src/modules/open-api/request.js
+++ src/modules/open-api/request.js
@@ -10,43 +10,60 @@ const serviceCode = {
     auction: 'Grid_20240625000000000654_1'
 }
 
-const getMarketCode = async () => {
-    const response = await axios.get(`/${serviceCode.market}/1/10`);
-    return response;
+const resultCode = {
+    "INFO-000": "정상 처리되었습니다.",
+    "INFO-100": "인증키가 유효하지 않습니다. 인증키가 없는 경우, 인증키를 신청하세요",
+    "INFO-200": "공개되지 않은 데이터이거나 데이터 정보가 없습니다.",
+    "ERROR-300": "기본값이 누락되었습니다. 요청인자를 참고하십시오.",
+    "ERROR-301": "파일타입 값이 누락 혹은 유효하지 않습니다. 요청인자 중 TYPE를 확인하십시오.",
+    "ERROR-310": "해당하는 서비스를 찾을 수 없습니다.",
+    "ERROR-331": "요청시작위치 값을 확인하십시오. 요청인자 중 START_INDEX를 확인하십시오.",
+    "ERROR-332": "요청종료위치 값을 확인하십시오. 요청인자 중 END_INDEX를 확인하십시오.",
+    "ERROR-333": "요청위치 값의 타입이 유효하지 않습니다. 요청위치 값은 정수를 입력하세요",
+    "ERROR-334": "요청종료위치 보다 요청시작위치가 더 큽니다. 요청시작조회건수는 정수를 입력하세요.",
+    "ERROR-335": "샘플데이터(샘플키:sample)는 한번에 최대 5건을 넘을 수 없습니다. 요청시작위치와 요청종료위치 값은 1~5 사이만 가능합니다.",
+    "ERROR-336": "데이터요청은 한번에 최대 1000건을 넘을 수 없습니다. 요청종료위치에서 요청시작위치를 뺀 값이 1000을 넘지 않도록 수정하세요.",
+    "ERROR-340": "필수 파라미터가 누락되었습니다.",
+    "ERROR-341": "필수 파라미터 데이터 형식이 맞지 않거나 크기가 맞지 않습니다.",
+    "ERROR-342": "선택 파라미터 데이터 형식이 맞지 않거나 크기가 맞지 않습니다.",
+    "ERROR-350": "일 요청 횟수를 초과하였습니다.",
+    "ERROR-500": "서버 오류입니다. 지속적으로 발생 시 관리자에게 문의 바랍니다.",
+    "ERROR-600": "데이터베이스 연결 오류입니다. 지속적으로 발생 시 관리자에게 문의 바랍니다.",
+    "ERROR-601": "SQL 문장 오류입니다. 지속적으로 발생 시 관리자에게 문의 바랍니다."
+}
+
+const getMarketCode = async (start, end) => {
+    // TODO: 예외처리 필요
+    try {
+        const response = await axios.get(`/${serviceCode.market}/${start}/${end}`);
+    } catch (e) {
+
+    }
 };
 
-const getCorporateCode = async() => {
+const getCorporateCode = async(start, end) => {
     const response = await axios('/code/cmp.do');
     return response.data;
 }
 
-const getProductCode = async(pageNo) => {
-    const response = await axios.get('/code/good.do', {
-        params: {
-            pageNo
-        }
-    })
-    
-    return response.data;
+const getProductCode = async(start, end) => {
+
 }
 
 
 const getAuction = async(day, start, end, market, large, mid, small) => {
-    const response = await axios.get(`http://${process.env.PUBLIC_API_URL}/${start}/${end}?SALEDATE=${day}&WHSALCD=${market}${large ? '&LARGE=' + large : ''}${large ? '&MID=' + mid : ''}${large ? '&SMALL=' + small : ''}`);
-    if(response.data.Grid_20240625000000000654_1.result.code !== 'INFO-000') {
-        // 임시 예외 처리
-        console.error(response.data.Grid_20240625000000000654_1.result.message);
-        return [];
-    }
-    return response.data.Grid_20240625000000000654_1.row;
     
 }
 
+// TODO: 데이터 변환 필요
+const getData = (response, code) => {
 
+}
 
 
 module.exports = {
     serviceCode,
+    resultCode,
     getMarketCode,
     getProductCode,
     getCorporateCode,
--- test/modules/open-api/request.test.js
+++ test/modules/open-api/request.test.js
@@ -2,13 +2,17 @@ const openApi = require('@/modules/open-api/request');
 
 
 test('도매시장 코드 조회', async () => {
-    const result = await openApi.getMarketCode();
-    expect(result.data[`${openApi.serviceCode.market}`].result.code).toEqual('INFO-000');
+    const result = await openApi.getMarketCode(0, 0);
+
+    expect(result).toHaveProperty('data');
+    expect(result.data).toHaveProperty(openApi.serviceCode.market);
+    expect(result.data[openApi.serviceCode.market]).toHaveProperty('result.code', 'INFO-000');
+
+    expect(result.data[openApi.serviceCode.market]).toHaveProperty('result.code', 'INFO-000').toMatchSnapshot();
 });
 
-test('법인 코드 조회', () => {});
+test('법인 코드 조회', async () => {});
 
-test('품목 코드 조회', () => {});
+test('품목 코드 조회', async  () => {});
 
-test('경락 정보 조회', () => {});
-
+test('경락 정보 조회', async  () => {});
