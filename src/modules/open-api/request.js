3a43f6ef4a1f9fb59bb7c62ef566265d42698c1e
윾승도 <yus174113@gmail.com>
2025-03-23 13:06:50 +0900

    feat: 품목 조회 1

--- src/modules/open-api/request.js
+++ src/modules/open-api/request.js
@@ -1,62 +1,59 @@
 const axios = require('axios');
 const Exception = require('../../exception/exception');
-const {HttpStatusCode} = require("axios");
+const { HttpStatusCode } = require('axios');
 require('dotenv').config();
 
-axios.defaults.baseURL = `${process.env.API_URL}/${process.env.API_KEY}/json`
-
+axios.defaults.baseURL = `${process.env.API_URL}/${process.env.API_KEY}/json`;
 
 const serviceCode = {
     market: 'Grid_20240625000000000661_1',
     product: 'Grid_20240626000000000668_1',
     coporate: 'Grid_20240626000000000662_1',
-    auction: 'Grid_20240625000000000654_1'
-}
+    auction: 'Grid_20240625000000000654_1',
+};
 
 const MAX_COUNT = 999;
 const AXIOS_ERROR = 'OPEN API 오류 발생';
 
-
-const getMarketCode = async (start) => {
-    return await axios.get(`/${serviceCode.market}/${start}/${MAX_COUNT}`)
+const getMarketCode = async start => {
+    return await axios(`/${serviceCode.market}/${start}/${MAX_COUNT}`)
         .then(response => checkReqeust(response, serviceCode.market))
-        .then(response => response.data[serviceCode.market])
-        .catch((e) => {
+        .catch(e => {
             throw new Exception(AXIOS_ERROR + ': 도매시장');
         });
 };
 
-const getCorporateCode = async(start) => {
+const getCorporateCode = async start => {
     return await axios(`/${serviceCode.coporate}/${start}/${MAX_COUNT}`)
         .then(response => checkReqeust(response, serviceCode.coporate))
-        .then(response =>  response.data[serviceCode.coporate])
-        .catch((e) => { throw new Exception(AXIOS_ERROR + ': 법인'); });
+        .catch(e => {
+            throw new Exception(AXIOS_ERROR + ': 법인');
+        });
+};
 
-}
+const getProductCode = async start => {
+    const response = await axios(`${serviceCode.product}/${start}/${MAX_COUNT}`)
+        .then(response => checkReqeust(response, serviceCode.product))
+        .catch(e => {
+            throw new Exception(AXIOS_ERROR + ': 품목');
+        });
+    console.log(response);
+};
 
-const getProductCode = async(start, end) => {
-
-}
-
-
-const getAuction = async(day, start, end, market, large, mid, small) => {
-
-}
+const getAuction = async (day, start, end, market, large, mid, small) => {};
 
 // OpenAPI 요청 예외 처리
 const checkReqeust = (response, service) => {
-    if(!response.data[service] && response.data.result.code !== 'INFO-000') {
+    if (!response.data[service] && response.data.result.code !== 'INFO-000') {
         throw new Exception(response.data.result.message, HttpStatusCode.InternalServerError);
     }
-    return response;
-}
-
-
+    return response.data[service];
+};
 
 module.exports = {
     serviceCode,
     getMarketCode,
     getProductCode,
     getCorporateCode,
-    getAuction
+    getAuction,
 };
--- src/service/productService.js
+++ src/service/productService.js
@@ -1,63 +1,47 @@
-const openApi = require("../modules/open-api/request");
+const openApi = require('../modules/open-api/request');
 const { Products } = require('../database/schema/productSchema');
 const fs = require('fs');
 
 const productCount = 17553 / 1000 + 1;
 
 const update = async () => {
-     // 1. 데이터 요청
-    let response =  [];
+    const res = await openApi.getProductCode(0, 10);
+};
 
-    for(let i = 1; i <= productCount; i++) {
-        response.push(await openApi.getProductCode(i).then(res => res.data));
-    }
-
-     // 2. 데이터 정렬
-    response = response
-        .flat(2)
-        .sort((a, b) => a.rn - b.rn);
-
-     // 3. 데이터 구죠 변경
-    let result = toDocument(response);
-
-     // 4. MongoDB에 삽입
-    await Products.insertMany(result);
-}
-
-const toDocument = (array) => {
+const toDocument = array => {
     const result = [];
-    for(const product of array) {
-         const tmp = {};
-         if(product.mid === "00") {
+    for (const product of array) {
+        const tmp = {};
+        if (product.mid === '00') {
             tmp.code = product.large;
             tmp.name = product.largeName;
             tmp.mid = [];
 
-             result.push(tmp);
-         } else if(product.small === "00") {
-             tmp.code = product.mid;
-             tmp.name= product.midName;
-             tmp.small = [];
+            result.push(tmp);
+        } else if (product.small === '00') {
+            tmp.code = product.mid;
+            tmp.name = product.midName;
+            tmp.small = [];
 
-             result[result.length - 1].mid.push(tmp);
-         } else {
-             tmp.name = product.goodName;
-             tmp.code = product.small;
-             const midLength = result[result.length - 1].mid.length - 1;
-             result[result.length - 1].mid[midLength].small.push(tmp);
-          }
-     }
+            result[result.length - 1].mid.push(tmp);
+        } else {
+            tmp.name = product.goodName;
+            tmp.code = product.small;
+            const midLength = result[result.length - 1].mid.length - 1;
+            result[result.length - 1].mid[midLength].small.push(tmp);
+        }
+    }
     return result;
-}
+};
 
 const get = async () => {
-    if(!await Products.exists()) {
+    if (!(await Products.exists())) {
         await update();
     }
 
     return await Products.find();
-}
+};
 
 module.exports = {
-   get,
-}
+    get,
+};
