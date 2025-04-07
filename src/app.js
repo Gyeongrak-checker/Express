50438da87237ef68b5940ab75515f9c1583a1497
윾승도 <yus174113@gmail.com>
2025-04-07 19:48:48 +0900

    fix: mongoDB에 데이터 없으면 자동 저장장

--- src/app.js
+++ src/app.js
@@ -9,9 +9,8 @@ dotenv.config();
 const markets = require('./routes/markets');
 const products = require('./routes/product');
 const auction = require('./routes/auction');
-const {HttpStatusCode} = require("axios");
-const Exception = require('./exception/exception');
 
+const { HttpStatusCode } = require('axios');
 
 const app = express();
 
@@ -23,14 +22,13 @@ app.use(cookieParser());
 // 라우터 설정
 app.use('/markets', markets);
 app.use('/products', products);
-app.use('/auction', auction)
+app.use('/auction', auction);
 
 // 데이터베이스 연결
 connect();
 
-
 // 404 핸들러 (모든 라우터 등록 이후에 위치)
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     next(createError(404));
 });
 
@@ -39,12 +37,8 @@ app.use((err, req, res, next) => {
     console.error(err);
     return res.status(err.status || HttpStatusCode.InternalServerError).json({
         code: err.status || HttpStatusCode.InternalServerError,
-        message: err.message || "서버 오류 발생"
+        message: err.message || '서버 오류 발생',
     });
-
-
-
-
 });
 
 module.exports = app;
--- src/database/server.js
+++ src/database/server.js
@@ -1,10 +1,26 @@
 const mongoose = require('mongoose');
+const { Product } = require('./schema/productSchema');
+const { Market } = require('./schema/marketSchema');
+
+const marketService = require('../service/marketService');
+const productService = require('../service/productService');
 
 const connect = () => {
     if (process.env.NODE_ENV !== 'production') {
-        mongoose.set('debug', false);
+        mongoose.set('debug', true);
     }
-    mongoose.connect(`mongodb://localhost:27017/market`).then(() => console.log('mongoDB connected'));
+    mongoose
+        .connect(`mongodb://localhost:27017/market`)
+        .then(() => console.log('mongoDB connected'))
+        .then(async () => {
+            if (!Product.exists()) {
+                await productService.save();
+            }
+
+            if (!Market.exists()) {
+                await marketService.save();
+            }
+        });
 };
 
 module.exports = connect;
